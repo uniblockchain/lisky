57bd5ff42361307cc5d60ed7f9ac139f60c9bdac
tosch110 <tobias@lightcurve.io>
2018-01-26 17:15:51 +0100

    :white_check_mark: Add verify message test functions

--- test/steps/crypto/1_given.js
+++ test/steps/crypto/1_given.js
@@ -18,6 +18,16 @@ import cryptoInstance from '../../../src/utils/cryptoModule';
 import * as inputUtils from '../../../src/utils/input/utils';
 import { getFirstQuotedString, getQuotedStrings } from '../utils';
 
+export function aMessageWithAPublicKeyAndASignature() {
+	const [message, publicKey, signature] = getQuotedStrings(
+		this.test.parent.title,
+	);
+
+	this.test.ctx.message = message;
+	this.test.ctx.publicKey = publicKey;
+	this.test.ctx.signature = signature;
+}
+
 export function theMessageUnderThePassphraseHasSignature() {
 	const signature = getFirstQuotedString(this.test.parent.title);
 	this.test.ctx.signature = signature;
@@ -40,16 +50,24 @@ export function aCryptoInstanceHasBeenInitialised() {
 		'getKeys',
 		'getAddressFromPublicKey',
 		'signMessage',
+		'verifyMessage',
 	].forEach(methodName => cryptoInstance[methodName].returns(cryptoResult));
 
 	this.test.ctx.cryptoResult = cryptoResult;
 	this.test.ctx.cryptoInstance = cryptoInstance;
 }
 
-export function aSenderPublicKey() {
-	const senderPublicKey = getFirstQuotedString(this.test.parent.title);
-	this.test.ctx.senderPublicKey = senderPublicKey;
+export function aSignature() {
+	const signature = getFirstQuotedString(this.test.parent.title);
+	this.test.ctx.signature = signature;
 }
+
+export function aPublicKey() {
+	const publicKey = getFirstQuotedString(this.test.parent.title);
+	this.test.ctx.publicKey = publicKey;
+}
+
+export const aSenderPublicKey = aPublicKey;
 
 export function aNonce() {
 	const nonce = getFirstQuotedString(this.test.parent.title);
